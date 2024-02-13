40a1e6d2263144263f6ace96366a2e9b53fa5554
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-02-13 20:11:59 +0000

    Add Spotify API handling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 import SearchBar from "../SearchBar/SearchBar";
+import { Spotify } from "../../util/Spotify/Spotify";
 
 function App() {
 
@@ -66,6 +67,7 @@ function App() {
   }
 
   function search(term) {
+    Spotify.search(term).then(result => setSearchResults(result))
     console.log(term)
   }
 
